00e472c1eaf507c194a52287ad2f035439193c4f
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-06-02 12:13:48 +0400

    initial commet

---
+++ demo/src/main/java/om/tra/demo/Controller.java
@@ -0,0 +1,31 @@
+package om.tra.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class Controller {
+    @GetMapping("greet")
+    public String greeting(@RequestParam String name){
+        return "Hi " + name ;
+    }
+
+    @GetMapping("Books")
+    public List<Book> Book(){
+
+        return Arrays.asList(
+           new Book("The Hobbit", "221"),
+           new Book("Animal Farm", "332"),
+           new Book("The Hunger Games", "442"),
+           new Book("The Diary of a Young Girl", "553")
+        );
+    }
+
+}
+
